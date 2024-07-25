c310bd245f592333807def1da67da97c445e70db
Anna <121625797+itsannoo@users.noreply.github.com>
2024-07-26 01:17:53 +0700

    Update auth.js
    perubahan pada auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const User = require('../models/user'); 
 
 // Register
 router.post('/register', async (req, res) => {
