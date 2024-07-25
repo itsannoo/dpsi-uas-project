46ddf7ca91a717c46d9c42989a93fbb81ef0c6f5
itsannoo <moli57212@gmail.com>
2024-07-26 03:23:32 +0700

    push

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-    const token = req.header('x-auth-token');
-    if (!token) {
-        return res.status(401).json({ message: 'No token, authorization denied' });
-    }
+  const token = req.header('x-auth-token');
+  console.log(`Token received: ${token}`);
+  if (!token) {
+      return res.status(401).json({ message: 'No token, authorization denied' });
+  }
 
-    try {
-        const decoded = jwt.verify(token, 'your_jwt_secret');
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-        res.status(401).json({ message: 'Token is not valid' });
-    }
+  try {
+      const decoded = jwt.verify(token, 'your_jwt_secret');
+      console.log('Token decoded:', decoded);
+      req.user = decoded.user;
+      next();
+  } catch (err) {
+      console.log('Token verification failed:', err.message);
+      res.status(401).json({ message: 'Token is not valid' });
+  }
 };
 
 const authorize = (roles) => {
