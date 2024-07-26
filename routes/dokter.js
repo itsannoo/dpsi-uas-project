eda9fb66540bcbdc9594d296cd2c67319f1398ae
Anna <121625797+itsannoo@users.noreply.github.com>
2024-07-26 16:27:49 +0700

    Update dokter.js

--- routes/dokter.js
+++ routes/dokter.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const { getDb } = require('../models/index');
 const { authenticate, authorize } = require('../middleware/auth');
 
-
+//authenticate ini adalah middleware yang biasanya digunakan untuk memverifikasi identitas pengguna yang membuat permintaan. 
+//Middleware ini akan memeriksa apakah permintaan datang dari pengguna yang terautentikasi (misalnya, dengan memeriksa token otentikasi dalam header permintaan).
+//Jika autentikasi gagal, permintaan akan dihentikan dan pengguna akan menerima respons kesalahan.
+//authorize(['admin']) Ini adalah middleware lain yang digunakan untuk memeriksa apakah pengguna yang telah diautentikasi memiliki izin yang sesuai untuk mengakses sumber daya. 
+//Dalam hal ini, middleware ini memeriksa apakah pengguna memiliki hak akses yang terkait dengan peran 'dpa'. 
+//Biasanya, fungsi authorize akan memeriksa peran atau hak akses pengguna yang sudah diautentikasi dan membandingkannya dengan hak akses yang diperlukan untuk melakukan tindakan ini. 
+//Jika pengguna tidak memiliki izin yang diperlukan, permintaan akan dihentikan, dan pengguna akan menerima respons kesalahan.
 // Create a new dokter
 router.post('/', authenticate, authorize(['admin']), async (req, res) => {
     try {
